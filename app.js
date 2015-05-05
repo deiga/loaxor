fb7d9e2b501f7612c9f1fc6415fd8f8878a099e6
Timo Sand <timo.j.sand@gmail.com>
2015-05-05 16:05:13 +0300

    DATA STORAGE Adds storage of stream channels to Redis

--- app.js
+++ app.js
@@ -6,16 +6,26 @@ var irc = require("irc-js");
 var config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
 var twitch = require("twitch.tv");
 var log = require("lugg")("app");
+var redis = require("redis");
+var url = require("url");
+var _ = require("lodash");
+
+var redisURL = url.parse(process.env.REDISCLOUD_URL || config.redisurl);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
 
 var streamers = {};
 
-var initialiseStreamers = function () {
-  var channelNames = config.channels;
-  channelNames.forEach(function (channelName) {
-    if (!streamers.hasOwnProperty(channelName)) {
-      streamers[channelName] = {live: false};
+var initialiseStreamers = function (args) {
+  log.info("Initialising streamers");
+  var streamerKeys = _.map(args, function (streamerName) { return "streamer:" + streamerName; });
+  client.sadd("streamers", streamerKeys);
+  _.each(streamerKeys, function (key) {
+    if (!client.exists(key)) {
+      client.hset(key, "live", false);
     }
   });
+  log.info("Initialisation done");
 };
 
 var updateStreamersStatus = function (channel) {
@@ -30,28 +40,33 @@ var updateStreamersStatus = function (channel) {
     streams.forEach( function (stream) {
       var streamer = stream.channel.name;
       onlineStreamers.push(streamer);
-      if (!streamers[streamer].live) {
+      if (!client.hget("streamer:" + streamer, "live")) {
         log.info(streamer + " just went live!");
         channel.say(streamer + " began streaming " + stream.game + ", " + stream.channel.status + ": " + stream.channel.url);
       }
-      streamers[streamer].live = true;
+      client.hset("streamer:" + streamer, "live", true);
     });
     var offlineStremers = config.channels.filter(function(streamer) { return onlineStreamers.indexOf(streamer) < 0; });
     offlineStremers.forEach(function (streamer) {
-      if (streamers[streamer].live) {
-        streamers[streamer].live = false;
+      if (client.hget("streamer:" + streamer, "live")) {
+        client.hset("streamer:" + streamer, "live", false);
       }
     });
-    log.debug(streamers);
+    var streamers = client.smembers("streamers");
+    var output = [];
+    _.each(streamers, function (streamer) {
+      output.push(client.dump(streamer));
+    });
+    log.debug(output);
   });
 };
 
 var bot = new irc.Client(config.irc);
-initialiseStreamers();
-
+initialiseStreamers(config.channels);
+log.info("Connecting to IRC");
 bot.connect(function (client) {
   log.info("loaxor connected");
-  client.join("#leftoutsidealone", function (err, channel) {
+  client.join(config.irc.channel, function (err, channel) {
     if (err) {
       log.error("Could not join channel :(", err);
       return;
