c36221a6026a3f4eb17a4c27e2d3553104258a45
Timo Sand <timo.j.sand@gmail.com>
2015-05-09 23:03:55 +0300

    STREAMERS Updates logic to correctly update and use redis stored streamers

--- app.js
+++ app.js
@@ -13,25 +13,28 @@ var _ = require("lodash");
 var redisURL = url.parse(process.env.REDISCLOUD_URL || config.redisurl);
 var redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 redisClient.auth(redisURL.auth.split(":")[1]);
-
-var streamers = {};
+var channels = [];
 
 var initialiseStreamers = function (args) {
   log.info("Initialising streamers");
   var streamerKeys = _.map(args, function (streamerName) { return "streamer:" + streamerName; });
   redisClient.sadd("streamers", streamerKeys);
   _.each(streamerKeys, function (key) {
-    redisClient.exists(key, function(_, result) {
+    redisClient.exists(key, function(err, result) {
       if (!result) {
         redisClient.hset(key, "live", false);
       }
     });
   });
+  redisClient.smembers("streamers", function (err, streamers) {
+    channels = _.map(streamers, function (streamer) { return streamer.split(":")[1]; });
+    log.info("Channels:", channels);
+  });
   log.info("Initialisation done");
 };
 
 var streamerLiveStatus = function (streamer, cb) {
-  redisClient.hget("streamer:" + streamer, "live", function (_, status) {
+  redisClient.hget("streamer:" + streamer, "live", function (err, status) {
     cb(typeof status === "boolean" ? status : status === "true");
   });
 };
@@ -43,39 +46,34 @@ var updateStreamersStatus = function (channel) {
       log.error("Something bad happened :(", err);
       return;
     }
-    log.debug("Response: ", res);
+    log.debug("Online channels response: ", res);
+
     var streams = res.streams;
-    log.debug("Streams: ", streams);
+    log.debug("Online channels streams: ", streams);
     var onlineStreamers = [];
-    streams.forEach( function (stream) {
+    _.each(streams, function (stream) {
       var streamer = stream.channel.name;
       onlineStreamers.push(streamer);
-      var liveStatus = null;
       streamerLiveStatus(streamer, function (status) {
-        liveStatus = status;
+        log.debug("Streamer live status:", streamer, status);
+        if (!status) {
+          log.info(streamer + " just went live!");
+          channel.say(streamer + " began streaming '" + stream.game + "', " + stream.channel.status + ": " + stream.channel.url);
+        }
+        redisClient.hset("streamer:" + streamer, "live", true);
       });
-      log.debug("Streamer live status:", streamer, liveStatus);
-      if (!liveStatus) {
-        log.info(streamer + " just went live!");
-        channel.say(streamer + " began streaming '" + stream.game + "', " + stream.channel.status + ": " + stream.channel.url);
-      }
-      redisClient.hset("streamer:" + streamer, "live", true);
     });
-    var offlineStremers = config.channels.filter(function(streamer) { return onlineStreamers.indexOf(streamer) < 0; });
-    offlineStremers.forEach(function (streamer) {
-      var liveStatus = null;
-      streamerLiveStatus(streamer, function (status) {
-        liveStatus = status;
-      });
-      if (liveStatus) {
+
+    var offlineStreamers = channels.filter(function(streamer) { return onlineStreamers.indexOf(streamer) < 0; });
+    log.debug("Offline streamers:", offlineStreamers);
+    _.each(offlineStreamers, function (streamer) {
         redisClient.hset("streamer:" + streamer, "live", false);
-      }
     });
-    var streamers = redisClient.smembers("streamers");
-    log.debug("Streamers:", streamers);
-    _.each(streamers, function (streamer) {
-      redisClient.hgetall(streamer, function (_, results) {
-        log.debug("Streamer output:", results);
+
+    log.debug("Streamers:", channels);
+    _.each(channels, function (streamer) {
+      redisClient.hgetall("streamer:"+streamer, function (err, results) {
+        log.debug("Streamer output:", streamer, results);
       });
     });
   });
@@ -91,11 +89,11 @@ bot.connect(function (client) {
       log.error("Could not join channel :(", err);
       return;
     }
-    log.info("Joined #leftoutsidealone");
+    log.info("Joined " + channel.name);
+    updateStreamersStatus(channel);
     setInterval(updateStreamersStatus, 30000, channel);
   });
 });
-log.info(streamerLiveStatus("streamer:deiga", console.log));
 bot.match("INVITE", function(msg) {
   msg.reply("Joining.");
   bot.join(msg.params[1]);
